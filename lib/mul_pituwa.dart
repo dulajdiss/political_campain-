f86f5312e0ec371c56012aaa033e69f32a3aeae7
Dulaj Dissanayake <126046538+dulajdiss@users.noreply.github.com>
2023-11-25 21:41:56 +0530

    created mul pituwa

---
+++ lib/mul_pituwa.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MulPituwa extends StatefulWidget {
+  const MulPituwa({super.key});
+
+  @override
+  State<MulPituwa> createState() => _MulPituwaState();
+}
+
+class _MulPituwaState extends State<MulPituwa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("fuck")),
+    );
+  }
+}
