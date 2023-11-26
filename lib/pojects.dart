52f681a0b7ac5aaa5f4ebe048dfbe5559e34a6e7
Dulaj Dissanayake <126046538+dulajdiss@users.noreply.github.com>
2023-11-26 21:28:42 +0530

    created this for the drawer

---
+++ lib/pojects.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/landing_page.dart';
+
+class Projects extends StatelessWidget {
+  const Projects({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded() objects takes all available space, and each Expanded gets the
+    // portion of space according to it's flex.
+    // SizedBox() instead has fixed height/width.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text("අනුරාධ ජයරත්න"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const LandingPage(
+                    title: '',
+                  ),
+                ),
+              );
+              // Perform search action here
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: Text("Projects"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
