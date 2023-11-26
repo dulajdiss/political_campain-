617eb8cfa21e74a1e39b44297c058f52769a6b97
Dulaj Dissanayake <126046538+dulajdiss@users.noreply.github.com>
2023-11-26 21:23:50 +0530

    removed unwanted codes

--- lib/mul_pituwa.dart
+++ lib/mul_pituwa.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/landing_page.dart';
 
-class MulPituwa extends StatefulWidget {
+class MulPituwa extends StatelessWidget {
   const MulPituwa({super.key});
 
   @override
-  State<MulPituwa> createState() => _MulPituwaState();
-}
-
-class _MulPituwaState extends State<MulPituwa> {
-  @override
   Widget build(BuildContext context) {
+    // Expanded() objects takes all available space, and each Expanded gets the
+    // portion of space according to it's flex.
+    // SizedBox() instead has fixed height/width.
     return Scaffold(
-      body: Center(child: Text("fuck")),
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text("අනුරාධ ජයරත්න"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const LandingPage(
+                    title: '',
+                  ),
+                ),
+              );
+              // Perform search action here
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: Text("Mul Pituwa"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
